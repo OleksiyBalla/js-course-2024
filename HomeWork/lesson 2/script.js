fac869d823d70e706c4ecfbbdefdb75203ebcf82
OleksiyBalla <oleksiy_balla@yahoo.com>
2024-09-30 09:18:24 +0300

    lesson 2

---
+++ HomeWork/lesson 2/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const name = "Oleksii";
+
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const btn3 = document.querySelector("#btn3");
+const btn4 = document.querySelector("#btn4");
+const text = document.querySelector("#text");
+
+btn1.addEventListener("click", () => {
+    text.classList.toggle("hidden");
+    text.classList.remove("color");
+    btn2.classList.toggle("hidden");
+});
+btn2.addEventListener("click", () => {
+    text.classList.add("color");
+});
+btn3.addEventListener("click", () => {
+    alert(new Date());
+});
+btn4.addEventListener('click', () => {
+    alert(`Привіт, ${name}`)
+}); 
